f55e51a582ab935eb2371932f86afe228fe09fc6
Jason <jason0101@estudiantec.cr>
2024-10-04 13:09:37 -0600

    Creacion del grafo

--- TankAttack.cpp
+++ TankAttack.cpp
@@ -1,180 +1,89 @@
-// TankAttack.cpp : Define el punto de entrada de la aplicación.
-//
+#include <windows.h>
+#include <cstdlib>  // Para srand y rand
+#include <ctime>    // Para time()
 
-#include "framework.h"
-#include "TankAttack.h"
+// Tamaño del grafo (4x4)
+const int N = 4;
 
-#define MAX_LOADSTRING 100
+// Definir la matriz del grafo ('O' para celdas vacías, 'X' para obstáculos)
+char graph[N][N];
 
-// Variables globales:
-HINSTANCE hInst;                                // instancia actual
-WCHAR szTitle[MAX_LOADSTRING];                  // Texto de la barra de título
-WCHAR szWindowClass[MAX_LOADSTRING];            // nombre de clase de la ventana principal
+// Prototipos
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
-// Declaraciones de funciones adelantadas incluidas en este módulo de código:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: Colocar código aquí.
-
-    // Inicializar cadenas globales
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_TANKATTACK, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // Realizar la inicialización de la aplicación:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TANKATTACK));
-
-    MSG msg;
-
-    // Bucle principal de mensajes:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+// Generar obstáculos aleatorios en el grafo
+void GenerateGraph() {
+    srand(time(0));  // Inicializar generador de números aleatorios
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (rand() % 5 == 0) {
+                graph[i][j] = 'X';  // Obstáculo
+            }
+            else {
+                graph[i][j] = 'O';  // Celda vacía
+            }
         }
     }
-
-    return (int) msg.wParam;
 }
 
+// Crear la ventana
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
+    WNDCLASS wc = { 0 };
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"TankAttackWindow";
+    wc.lpfnWndProc = WindowProcedure;
 
+    if (!RegisterClass(&wc))
+        return -1;
 
-//
-//  FUNCIÓN: MyRegisterClass()
-//
-//  PROPÓSITO: Registra la clase de ventana.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
+    GenerateGraph();  // Generar el grafo
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+    CreateWindow(L"TankAttackWindow", L"Tank Attack Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, hInst, NULL);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TANKATTACK));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TANKATTACK);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    MSG msg = { 0 };
+    while (GetMessage(&msg, NULL, NULL, NULL)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-    return RegisterClassExW(&wcex);
+    return 0;
 }
 
-//
-//   FUNCIÓN: InitInstance(HINSTANCE, int)
-//
-//   PROPÓSITO: Guarda el identificador de instancia y crea la ventana principal
-//
-//   COMENTARIOS:
-//
-//        En esta función, se guarda el identificador de instancia en una variable común y
-//        se crea y muestra la ventana principal del programa.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Almacenar identificador de instancia en una variable global
+// Procedimiento de la ventana
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    switch (msg) {
+    case WM_CREATE: {
+        // Crear botones para representar el grafo
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                HWND hButton = CreateWindow(
+                    L"BUTTON",
+                    NULL,
+                    WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                    50 + j * 80, 50 + i * 80, 70, 70,
+                    hwnd, (HMENU)(i * N + j),
+                    (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+                    NULL
+                );
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  FUNCIÓN: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PROPÓSITO: Procesa mensajes de la ventana principal.
-//
-//  WM_COMMAND  - procesar el menú de aplicaciones
-//  WM_PAINT    - Pintar la ventana principal
-//  WM_DESTROY  - publicar un mensaje de salida y volver
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Analizar las selecciones de menú:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
+                // Cambiar el texto del botón dependiendo de si es obstáculo o celda vacía
+                if (graph[i][j] == 'X') {
+                    SetWindowText(hButton, L"X");
+                }
+                else {
+                    SetWindowText(hButton, L"O");
+                }
             }
         }
         break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Agregar cualquier código de dibujo que use hDC aquí...
-            EndPaint(hWnd, &ps);
-        }
-        break;
+    }
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, msg, wp, lp);
     }
     return 0;
-}
-
-// Controlador de mensajes del cuadro Acerca de.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
